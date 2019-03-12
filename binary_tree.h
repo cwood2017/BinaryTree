006ddf433624b2e6c43519cb0e0415bff3f8fe88
Nate J <grasshopper314159@gmail.com>
2019-03-12 14:45:52 -0600

    More defs added

--- binary_tree.h
+++ binary_tree.h
@@ -1,4 +1,5 @@
-
+#ifndef binaryT_H
+#define binaryT_H
 
 typedef struct binary_tree_struct
 {
@@ -6,5 +7,21 @@ typedef struct binary_tree_struct
   node* root;
 } binary_ tree;
 
-
+typedef struct node_struct
+{
+  int data;
+  struct node_struct* left;
+  struct node_struct* right;
+} node;
    
+   
+void initialize(binary_tree* bt);
+bool search(binary_tree* bt, int key);
+void insert(binary_tree* bt, int item);
+void printinorder(binary_tree* bt);
+void printpreorder(binary_tree* bt);
+void printpostorder(binary_tree* bt);
+int btsize(binary_tree* bt);
+int treeheight(binary_tree* bt);
+
+#endif
