5784d890174e8d42dc499bb3f73e04fe1ecc1a63
Cody Wood <cwood2017@icloud.com>
2019-03-13 14:12:27 -0600

    Initialize

---
+++ CSCI205/BinaryTree/BinaryTree-master/binary_tree.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "binaryT_H"
+
+void initialize(binary_tree* bt) {
+    bt->root = NULL;
+    bt->size = 0;
+    bt->heiht = 0;
+}
+
+bool search(binary_tree* bt, int key) {
+}
+
+void insert(binary_tree* bt, int item) {
+}
+
+void printinorder(binary_tree* bt) {
+}
+
+void printpreorder(binary_tree* bt) {
+}
+
+void printpostorder(binary_tree* bt) {
+}
+
+int btsize(binary_tree* bt) {
+}
+
+int treeheight(binary_tree* bt) {
+}
