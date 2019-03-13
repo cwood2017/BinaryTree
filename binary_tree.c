302f735a3ebb2dff605562e2342660c958929c8a
Cody Wood <cwood2017@icloud.com>
2019-03-13 14:29:53 -0600

    Initialize

--- binary_tree.c
+++ binary_tree.c
@@ -5,3 +5,37 @@
 #include "binary_tree.h"
 #include <stdbool.h>
 
+void initialize(binary_tree* bt)
+{
+    bt->root = NULL;
+    bt->size = 0;
+    bt->height = 0;
+}
+
+bool search(binary_tree* bt, int key)
+{
+}
+
+void insert(binary_tree* bt, int item)
+{
+}
+
+void printinorder(binary_tree* bt)
+{
+}
+
+void printpreorder(binary_tree* bt)
+{
+}
+
+void printpostorder(binary_tree* bt)
+{
+}
+
+int btsize(binary_tree* bt)
+{
+}
+
+int treeheight(binary_tree* bt)
+{
+}
